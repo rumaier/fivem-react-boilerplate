5f0967a3498dd00155034c5cfde17b5188468918
rumaier <148654629+rumaier@users.noreply.github.com>
2026-08-14 11:37:59 -0500

    feat: add setNuiFocus utility function to manage NUI focus state in non-browser environments

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -1,5 +1,12 @@
+import { fetchNui } from "./fetchNui";
+
 export const isEnvBrowser = (): boolean => {
   return !(window as any).invokeNative;
+};
+
+export const setNuiFocus = (focus: boolean) => {
+  if (isEnvBrowser()) return;
+  fetchNui('setNuiFocus', focus);
 };
 
 export const rem = (value: number): number => {
